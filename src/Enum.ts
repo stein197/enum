fd6ce575f85f70aa7a188731d51ee03dc1e3d700
Nail Gafarov <nil20122013@gmail.com>
2021-08-12 12:43:19 +0500

    Basic implementation: +field(Enum._name, Enum._values), +get(Enum.name, Enum.values), +method(Enum.from, Enum.objectsAreEqual)

--- src/Enum.ts
+++ src/Enum.ts
@@ -1,4 +1,38 @@
 export default class Enum<T extends {[key: string]: any}> {
 
+	private _name: string;
+	private static _values: Enum<{}>[];
+
+	public get name(): string {
+		if (!this._name)
+			this._name = Object.entries(this.constructor).filter(entry => entry[1] === this)[0][0];
+		return this._name;
+	}
+
+	public static get values(): Enum<{}>[] {
+		if (!this._values) {
+			const entries: [string, Enum<{}>][] = Object.entries(this);
+			this._values = new Array(entries.length);
+			for (const i in entries)
+				this._values[i] = entries[i][1];
+		}
+		return this._values;
+	}
+
 	protected constructor(public readonly properties: T) {}
+
+	public static from(data: string): Enum<{}>;
+
+	public static from<T>(data: T): Enum<{}>;
+
+	public static from<T>(data: string | T): Enum<{}> {
+		return typeof data === "string" ? (this as any)[data] : this.values.filter(entry => this.objectsAreEqual(data, entry.properties))[0]
+	}
+
+	private static objectsAreEqual<T>(obj1: T, obj2: T): boolean {
+		for (const [key] of Object.entries(obj1))
+			if ((obj1 as any)[key] !== (obj2 as any)[key])
+				return false;
+		return true;
+	}
 }
